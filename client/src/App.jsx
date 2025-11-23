cca53f7abe3fe96d8dd5dc97f7cd0615860560ca
ppenkof <overton@abv.bg>
2025-11-23 11:42:14 +0200

    Lift user state to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,26 @@ import Catalog from "./components/catalog/Catalog"
 import Details from "./components/details/Details"
 import GameCreate from "./components/game-create/GameCreate"
 import Register from "./components/register/Register"
+import { useState } from "react"
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const registerHandler = (email)=>{
+    setUser({email});
+  }
   
   return (
     <>
-      <Header />
+      <Header user={user}/>
 
       <Routes>
         <Route path="/" element = {<Home />} />
         <Route path="/games" element = {<Catalog />} />
         <Route path="/games/:gameId/details" element = {<Details />} />
         <Route path="/games/create" element = {<GameCreate />} />
-        <Route path="/register" element = {<Register />} />
+        <Route path="/register" element = {<Register user={user} onRegister={registerHandler}/>} />
       </Routes>
       
       <Footer /> 
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,12 +1,38 @@
-export default function Register(){
+
+
+export default function Register({
+    user,
+    onRegister
+}){
+    //const [user, setUser] = useState(null);
+    const registerSubmit = (formData) => {
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirm-password');
+
+        //Validation
+
+        if(!email || !password){
+        return alert('All fields are required!');
+        };
+
+        if(password !== confirmPassword){
+        return alert('Passwords don\'t match!');
+        }
+
+        //Register user
+        onRegister(email);
+
+    }
 
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" action={registerSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
-    
+    {user && <p>Registered as: {user.email}</p>}
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" placeholder="Your Email"/>
     
