f5bc6c4db97c5619cab8a238bb34fcb9654945e9
ppenkof <overton@abv.bg>
2025-11-29 18:46:42 +0200

    Create useForm hook and refactor register component

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,55 +1,100 @@
 import { useNavigate } from "react-router";
+import useForm from "../../hooks/useForm";
 
+export default function Register({ onRegister }) {
+  const navigate = useNavigate();
 
-export default function Register({
-    onRegister
-}){
-    const navigate = useNavigate();
-    const registerSubmit = (formData) => {
+  // After second exerise way with formData without real state management
+  // const registerSubmit = (formData) => {
 
-        const email = formData.get('email');
-        const password = formData.get('password');
-        const confirmPassword = formData.get('confirm-password');
+  //     const email = formData.get('email');
+  //     const password = formData.get('password');
+  //     const confirmPassword = formData.get('confirm-password');
 
-        //Validation
+  //     //Validation
 
-        if(!email || !password){
-        return alert('All fields are required!');
-        };
+  //     if(!email || !password){
+  //     return alert('All fields are required!');
+  //     };
 
-        if(password !== confirmPassword){
-        return alert('Passwords don\'t match!');
-        }
+  //     if(password !== confirmPassword){
+  //     return alert('Passwords don\'t match!');
+  //     }
 
-        try {
-            //Register user
-            onRegister(email, password);
-            navigate('/');
-        } catch (error) {
-            alert(error.message);
-        }
+  //     try {
+  //         //Register user
+  //         onRegister(email, password);
+  //         navigate('/');
+  //     } catch (error) {
+  //         alert(error.message);
+  //     }
 
+  // }
+
+  const registerHandler = (values) => {
+    const { email, password, confirmPassword: confirmPassword } = values;
+    //Validation
+
+    if (!email || !password) {
+      return alert("All fields are required!");
     }
 
-    return (
-        <section id="register-page" className="content auth">
-            <form id="register" action={registerSubmit}>
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Register</h1>
+    if (password !== confirmPassword) {
+      return alert("Passwords don't match!");
+    }
 
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Your Email"/>
-    
-                    <label htmlFor="pass">Password:</label>
-                    <input type="new-password" name="password" id="register-password" placeholder="Password"/>
-    
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="new-password" name="confirm-password" id="confirm-password" placeholder="Repeat Password"/>
-    
-                    <input className="btn submit" type="submit" value="Register"/>
-                </div>
-            </form>
-        </section>
-    );
+    try {
+      //Register user
+      onRegister(email, password);
+      navigate("/");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const { 
+    register,
+    formAction, 
+} = useForm(registerHandler, {
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  return (
+    <section id="register-page" className="content auth">
+      <form id="register" action={formAction}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Register</h1>
+
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email")}
+            placeholder="Your Email"
+          />
+
+          <label htmlFor="pass">Password:</label>
+          <input
+            type="new-password"
+            {...register("password")}
+            id="register-password"
+            placeholder="Password"
+          />
+
+          <label htmlFor="con-pass">Confirm Password:</label>
+          <input
+            type="new-password"
+            {...register("confirmPassword")}
+            id="confirm-password"
+            placeholder="Repeat Password"
+          />
+
+          <input className="btn submit" type="submit" value="Register" />
+        </div>
+      </form>
+    </section>
+  );
 }
